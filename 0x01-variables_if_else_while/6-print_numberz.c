12a3e8e5ba0e488fdb592cd16db7190a54f48ee0
khobbytubz <khobbytubz@gmail.com>
2022-09-09 11:22:12 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <item.h>
+#include <stdlib.h>
+/**
+ * main - Entry level
+ *
+ * Return: always 0 (pass)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
