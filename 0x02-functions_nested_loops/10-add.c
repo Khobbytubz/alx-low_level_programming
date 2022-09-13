28aa6c25e53734faedb66bbcf511f4fde82e3707
khobbytubz <khobbytubz@gmail.com>
2022-09-13 11:48:58 -0700

    adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,10 @@
 
 /**
  * add - check main
+ * @a: An integer a
+ * @b: An integer b
  * Description: adds two integers and returns the result
- * @a: An int a
- * @b: An int b
- * return: result of a and b
+ * Return: result of a and b
  */
 int add(int a, int b)
 {
