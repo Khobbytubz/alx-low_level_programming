2c544a507d36af286c2656847762bc459e81657a
khobbytubz <khobbytubz@gmail.com>
2022-09-19 16:40:33 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,9 +9,7 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
+	for (i = 0; *(str + i ) != '\0'; i++)
+		_putchar(*(str + i));
 	_putchar('\n');
 }
